c40571d16ed82d4ec943fbe9401c3c3438286133
Joe Kim <josephkim0224@gmail.com>
2016-04-01 16:31:48 -0400

    170 course exercises

---
+++ 170_lesson_2/exercises.rb
@@ -0,0 +1,13 @@
+# 1. What are the required components of an HTTP request?
+#    What are the additional optional components?
+# Http method are path are required, but parameters, header, and body are not.
+
+# 2. What are the required components of an HTTP response? 
+#    What are the additional optional components?
+# Status code is required, but header and body are not.
+
+# 3. What determines whether a request should use GET or POST as its HTTP method?
+# GET is used to fetch data from a server, but POST is used to push/change data to server.
+
+# 4. Is the host component of a URL included as part of an HTTP request?
+# No, just the path and the parameters (if necessary).
---
+++ 170_lesson_3/url_comp_execises.rb
@@ -0,0 +1,32 @@
+# 1. Given the following URL:
+#    https://amazon.com/Double-Stainless-Commercial-Refrigerator/
+#    B60HON32?ie=UTF8&qid=142952676&sr=93&keywords=commercial+fridge
+# a. host = amazon.com
+# b. names of query: ie, qid, sr, keywords
+# c. values of query: UTF8, 142952676, 93, commercial+fridge
+# d. protocol: https
+# e. path = /Double-Stainless-Commercial-Refridgerator/B60HON32
+# f. port: 443 (but not contained in the URL)
+
+# 2. Add the port 3000 to the following URL:
+# http://amazon.com:3000/products/B60HON32?qid=142952676&sr=93
+
+# 3. Given the following URL:
+# => http://localhost:4567/todos/15
+# a. query parameters: none
+# b. protocol: http
+# c. path: /todos/15
+# d. host: localhost
+# e. port: 4567
+
+# 4. What are two different ways to encode a space in a query parameter?
+# %20 or +
+
+# 5. What character indicates the beginning of a URL's query parameters?
+# ?
+
+# 6. What character is used between the name and value of a query parameter?
+# =
+
+# 7. What character is used between multiple query parameters?
+# & 

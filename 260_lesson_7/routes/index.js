b4609a6b91fabbba60ca9423e22b7cd53a94b15c
Joe Kim <josephkim0224@gmail.com>
2016-08-15 18:12:10 -0400

    finished 260

---
+++ 260_lesson_7/routes/index.js
@@ -0,0 +1,15 @@
+var path = require("path"),
+    fs = require("fs"),
+    file_path = path.resolve(path.dirname(__dirname), "data/albums.json");
+
+function getAlbums() {
+  return JSON.parse(fs.readFileSync(file_path, "utf8")).data;
+}
+
+module.exports = function(router) {
+  router.get('/', function(req, res, next) {
+    res.render('index', { 
+      albums: getAlbums() 
+    });
+  });
+};
